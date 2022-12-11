14dde1ff779b5529dedb251f76e66bcc3f39c13f
Guilherme-tubaron <logingui@gmail.com>
2022-12-10 22:07:36 -0300

    a

---
+++ script.js
@@ -0,0 +1,20 @@
+const arvore = "sou uma arvore";
+let madeira;
+
+madeira = "sou uma madeira";
+
+console.log(arvore);
+console.log(madeira);
+
+madeira == arvore;
+
+console.log("");
+console.log(madeira);
+
+const valorUm = "2";
+const valorDois = "4";
+
+
+// parseInt tranforma uma variavel de outro tipo em um numero int
+console.log(parseInt(valorUm) + parseInt(valorDois));
+
